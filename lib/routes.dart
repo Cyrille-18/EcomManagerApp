341078f523ba67875dbf31fe37d8bec253c4e689
Cyrille-18 <cyrillepio@hotmail.com>
2025-04-24 22:41:19 +0000

    Feat(Routes): Add route for OrdersScreen to appRoutes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'screens/orders_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   '/home': (context) => const HomeScreen(),
+  '/orders': (context) => const OrdersScreen(),
 };
