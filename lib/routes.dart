c7561f7bb4c6eb8db1c95a8447a9a296538772e5
Cyrille-18 <cyrillepio@hotmail.com>
2025-05-06 16:55:49 +0000

    Feat(Routes): Add route for CategoriesScreen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:maplenou/screens/categories_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/orders_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   '/home': (context) => const HomeScreen(),
   '/orders': (context) => const OrdersScreen(),
+  '/categories': (context) => const CategoriesScreen(),
 };
