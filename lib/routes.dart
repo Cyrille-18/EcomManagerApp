57896d5d8ef517d6177b09fba1fd15bf7cb3a2ad
Cyrille-18 <cyrillepio@hotmail.com>
2025-05-26 18:29:54 +0000

    Feat(Routes): Add login route to appRoutes for navigation

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:maplenou/screens/addcategory_screen.dart';
 import 'package:maplenou/screens/categories_screen.dart';
+import 'package:maplenou/screens/login_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/orders_screen.dart';
 
@@ -8,5 +9,6 @@ final Map<String, WidgetBuilder> appRoutes = {
   '/home': (context) => const HomeScreen(),
   '/orders': (context) => const OrdersScreen(),
   '/categories': (context) => const CategoriesScreen(),
-  '/addcategory': (context) => const AddcategoryScreen()
+  '/addcategory': (context) => const AddcategoryScreen(),
+  '/login': (context) => const LoginScreen(),
 };
